c8e22cbf6d019397a7c5d91105dc7953de7bc5d6
Doris-Siu <doris.siu.my@gmail.com>
2022-11-03 12:23:05 +0000

    Complete level 200

--- script.js
+++ script.js
@@ -6,14 +6,50 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+
+  const searchBar = document.createElement("input");
+  searchBar.addEventListener("input", function () {
+    drawEpiCard(episodeList, searchBar.value.toLowerCase());
+  });
+  const searchResult = document.createElement("label");
+  searchResult.id = "searchResult";
+  searchBar.setAttribute("type", "string");
+
+  rootElem.innerHTML = `Got ${episodeList.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
+  rootElem.appendChild(searchBar);
+  rootElem.appendChild(searchResult);
+  drawEpiCard(episodeList);
+}
+function clearCards() {
+  document
+    .querySelectorAll("#root .grid-container")
+    .forEach((el) => el.remove());
+}
+function drawEpiCard(episodeList, searchBy) {
+  clearCards();
+  const rootElem = document.getElementById("root");
+
   const container = document.createElement("div");
   container.className = "grid-container";
-  rootElem.innerHTML = `Got ${episodeList.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a>`
-  episodeList.forEach((episode) => container.appendChild(getEpiCard(episode)));
-
+  let found = 0;
+  episodeList.forEach((episode) => {
+    if (
+      !searchBy ||
+      episode.summary.toLowerCase().includes(searchBy) ||
+      episode.name.toLowerCase().includes(searchBy)
+    ) {
+      found++;
+      container.appendChild(getEpiCard(episode));
+    }
+  });
   rootElem.appendChild(container);
+  const searchResult = document.getElementById("searchResult");
+  if (found > 0 && searchBy) {
+    searchResult.innerText = `Displaying ${found}/${episodeList.length} episodes`;
+  } else {
+    searchResult.innerText = "";
+  }
 }
-
 function formatVal(val) {
   return val.toString().padStart(2, "0");
 }
