cb8dc56c43e19c67c8ffe9b586d169e5a1ce3ba9
Doris-Siu <doris.siu.my@gmail.com>
2022-11-04 00:10:46 +0000

    Update level 300

--- script.js
+++ script.js
@@ -7,31 +7,42 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
-  //search bar and result
+  //search bar
   const searchBar = document.createElement("input");
   searchBar.id = "txtSearchBy";
+  searchBar.setAttribute("type", "string");
   searchBar.addEventListener("input", function () {
     drawEpiCard(episodeList);
   });
+
+  //search result
   const searchResult = document.createElement("label");
   searchResult.id = "searchResult";
-  searchBar.setAttribute("type", "string");
-
+  
+  // select/ddl
   const selectMenu = document.createElement("select");
   selectMenu.id = "selName";
+  
+  //add placeholder to ddl
   const optionPlaceHolderEl = document.createElement("option");
   optionPlaceHolderEl.innerText = "--Please Select--";
   selectMenu.appendChild(optionPlaceHolderEl);
+
+  //add options to ddl
   episodeList.forEach((episode) => {
     const optionEl = document.createElement("option");
     optionEl.innerText = `S${formatVal(episode.season)}E${formatVal(
       episode.number
     )} - ${episode.name}`;
-    selectMenu.addEventListener("change", function () {
-      drawEpiCard(episodeList);
-    });
+
     selectMenu.appendChild(optionEl);
   });
+  
+  //onchange event of ddl
+  selectMenu.addEventListener("change", function () {
+      drawEpiCard(episodeList);
+  });
+  
 
   rootElem.innerHTML = `Got ${episodeList.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
   rootElem.appendChild(selectMenu);
@@ -39,11 +50,13 @@ function makePageForEpisodes(episodeList) {
   rootElem.appendChild(searchResult);
   drawEpiCard(episodeList);
 }
+
 function clearCards() {
   document
-    .querySelectorAll("#root .grid-container")
+    .querySelectorAll("#root div.grid-container")
     .forEach((el) => el.remove());
 }
+
 function drawEpiCard(episodeList) {
   clearCards();
   //get search words
@@ -63,16 +76,22 @@ function drawEpiCard(episodeList) {
 
   let found = 0;
   episodeList.forEach((episode) => {
-    // if (optionEl && optionEl.innerText.includes(episode.name)){
-    //   container.appendChild(getEpiCard(episode));
-    // }
+
     if (
-      (selectedName == "--Please Select--" ||
-        selectedName.includes(episode.name)) &&
-      (!searchBy ||
-        episode.summary.toLowerCase().includes(searchBy) ||
-        episode.name.toLowerCase().includes(searchBy))
-    ) {
+      //ddl filter
+        (
+          selectedName === "--Please Select--" 
+          || selectedName.includes(episode.name)
+        ) 
+      &&
+      //search bar filter
+        (
+          !searchBy 
+          || episode.summary.toLowerCase().includes(searchBy) 
+          || episode.name.toLowerCase().includes(searchBy)
+        )
+    ) 
+    {
       found++;
       container.appendChild(getEpiCard(episode));
     }
@@ -85,6 +104,7 @@ function drawEpiCard(episodeList) {
     searchResult.innerText = "";
   }
 }
+
 function formatVal(val) {
   return val.toString().padStart(2, "0");
 }
