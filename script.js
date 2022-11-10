9e0aefa2ea355b67e12fb0a4c0018149489059dc
Doris-Siu <doris.siu.my@gmail.com>
2022-11-10 01:19:50 +0000

    Complete level 400

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
 let _localEpisodes = [];
-async function getAllEpisodes() {
-  if (_localEpisodes.length == 0) {
-    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
-    _localEpisodes = response.json();
+let _localShows = [];
+let _showDDLCtrlId = "selShow";
+let _epDDLCtrlId = "selName";
+function sortByName(arr) {
+  return arr.sort(function (a, b) {
+    var textA = a.name.toLowerCase();
+    var textB = b.name.toLowerCase();
+    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  });
+}
+async function getAllShowAsync() {
+  if (_localShows.length == 0) {
+    _localShows = sortByName(getAllShows());
   }
-  return _localEpisodes;
+  return true;
+}
+async function getAllEpisodesAsync(showId) {
+  if (!showId && _localShows.length == 0) {
+    console.error("Cannot get shows");
+  }
+  let currentShowId = showId ?? _localShows[0].id;
+  let localShowsEpisodes = {};
+  //use local storage to avoid api load
+  let localShowsEpisodesJsonStr = localStorage.getItem("localShowsEpisodes");
+  //localStorage has localShowsEpisodes Item and the item has currentshowId key
+  if (localShowsEpisodesJsonStr) {
+    localShowsEpisodes = JSON.parse(localShowsEpisodesJsonStr);
+    if (localShowsEpisodes[currentShowId]) {
+      //setup Episodes
+      _localEpisodes = localShowsEpisodes[currentShowId];
+      return true;
+    }
+  }
+  //if localStorage items have no key equals to currentShowId, call api
+  const response = await fetch(
+    "https://api.tvmaze.com/shows/" + currentShowId + "/episodes"
+  );
+  //save the result to localStorage
+  localShowsEpisodes[currentShowId] = await response.json();
+  localStorage.setItem(
+    "localShowsEpisodes",
+    JSON.stringify(localShowsEpisodes)
+  );
+  //setup Episodes
+  _localEpisodes = localShowsEpisodes[currentShowId];
+  return true;
 }
 //You can edit ALL of the code here
 async function setup() {
-  const allEpisodes = await getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  createSearchResult();
+  createShowMenu();
+  createEPMenu();
+  createSearchBar();
+  let allshowsResult = await getAllShowAsync();
+  if (allshowsResult) {
+    let allEpisodesResult = await refreshEpisodesDDL();
+    refreshShowMenu();
+    if (allEpisodesResult) {
+      drawEpiCard();
+    }
+  }
 }
+async function refreshEpisodesDDL(showId) {
+  await getAllEpisodesAsync(showId);
+  const selectMenu = document.getElementById(_epDDLCtrlId);
+  selectMenu.innerHTML = "";
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-
-  //search bar
-  const searchBar = document.createElement("input");
-  searchBar.id = "txtSearchBy";
-  searchBar.setAttribute("type", "string");
-  searchBar.addEventListener("input", function () {
-    drawEpiCard(episodeList);
-  });
-
-  //search result
-  const searchResult = document.createElement("label");
-  searchResult.id = "searchResult";
-
-  // select/ddl
-  const selectMenu = document.createElement("select");
-  selectMenu.id = "selName";
-
-  //add placeholder to ddl
   const optionPlaceHolderEl = document.createElement("option");
   optionPlaceHolderEl.innerText = "--Please Select--";
   selectMenu.appendChild(optionPlaceHolderEl);
 
-  //add options to ddl
-  episodeList.forEach((episode) => {
+  _localEpisodes.forEach((episode) => {
     const optionEl = document.createElement("option");
     optionEl.innerText = `S${formatVal(episode.season)}E${formatVal(
       episode.number
@@ -45,26 +78,76 @@ function makePageForEpisodes(episodeList) {
 
     selectMenu.appendChild(optionEl);
   });
+  drawEpiCard();
+}
+function refreshShowMenu() {
+  const selectShow = document.getElementById(_showDDLCtrlId);
+  selectShow.innerHTML = "";
+  //add options to ddl(select show)
+  _localShows.forEach((show) => {
+    const showEl = document.createElement("option");
+    showEl.innerText = show.name;
+    showEl.value = show.id;
+    selectShow.appendChild(showEl);
+  });
+}
+function createSearchResult() {
+  const rootElem = document.getElementById("root");
+  const resultP = document.createElement("p");
+  resultP.innerHTML = `Got ${_localEpisodes.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
+  rootElem.appendChild(resultP);
+}
+function createShowMenu() {
+  const rootElem = document.getElementById("root");
 
-  //onchange event of ddl
-  selectMenu.addEventListener("change", function () {
-    drawEpiCard(episodeList);
+  // select/ddl (select show)
+  const selectShow = document.createElement("select");
+  selectShow.id = _showDDLCtrlId;
+  selectShow.addEventListener("change", function () {
+    refreshEpisodesDDL(selectShow.value);
   });
 
-  rootElem.innerHTML = `Got ${episodeList.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
+  rootElem.appendChild(selectShow);
+}
+function createEPMenu() {
+  const rootElem = document.getElementById("root");
+  const selectMenu = document.createElement("select");
+  selectMenu.id = _epDDLCtrlId;
+  selectMenu.addEventListener("change", function () {
+    drawEpiCard();
+  });
   rootElem.appendChild(selectMenu);
+}
+function createSearchBar() {
+  const rootElem = document.getElementById("root");
+
+  //search bar
+  const searchBar = document.createElement("input");
+  searchBar.id = "txtSearchBy";
+  searchBar.setAttribute("type", "string");
+  searchBar.addEventListener("input", function () {
+    drawEpiCard();
+  });
+
+  //search result
+  const searchResult = document.createElement("label");
+  searchResult.id = "searchResult";
+
+  // const resultP = document.createElement("p");
+  // resultP.innerHTML = `Got ${_localEpisodes.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
+  // //rootElem.innerHTML = `Got ${_localEpisodes.length} episode(s). The data has (originally) come from <a href= "https://www.tvmaze.com/">TVMaze.com</a><br>`;
+
+  // rootElem.insertBefore(resultP, rootElem.firstChild);
   rootElem.appendChild(searchBar);
   rootElem.appendChild(searchResult);
-  drawEpiCard(episodeList);
+  //drawEpiCard();
 }
-
 function clearCards() {
   document
     .querySelectorAll("#root div.grid-container")
     .forEach((el) => el.remove());
 }
-
-function drawEpiCard(episodeList) {
+function drawEpiCard() {
   clearCards();
   //get search words
   const searchBar = document.getElementById("txtSearchBy");
@@ -82,7 +165,7 @@ function drawEpiCard(episodeList) {
   container.className = "grid-container";
 
   let found = 0;
-  episodeList.forEach((episode) => {
+  _localEpisodes.forEach((episode) => {
     if (
       //ddl filter
       (selectedName === "--Please Select--" ||
@@ -99,16 +182,14 @@ function drawEpiCard(episodeList) {
   rootElem.appendChild(container);
   const searchResult = document.getElementById("searchResult");
   if (found > 0 && searchBy) {
-    searchResult.innerText = `Displaying ${found}/${episodeList.length} episodes`;
+    searchResult.innerText = `Displaying ${found}/${_localEpisodes.length} episodes`;
   } else {
     searchResult.innerText = "";
   }
 }
-
 function formatVal(val) {
   return val.toString().padStart(2, "0");
 }
-
 function getEpiCard(episode) {
   const divEl = document.createElement("div");
   const titleEl = document.createElement("h1");
@@ -126,4 +207,4 @@ function getEpiCard(episode) {
   return divEl;
 }
 
-window.onload = setup;
+window.onload = setup();
